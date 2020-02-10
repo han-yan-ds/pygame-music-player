c6c60b3c2860350559d0af20adf92cd029e4336d
han-yan-ds <hanprogramming@gmail.com>
2020-02-10 15:04:30 -0700

    Put monolith into __main__

--- music_mixer.py
+++ music_mixer.py
@@ -14,30 +14,33 @@ def set_dir(path):
         os.mkdir(path)
         os.chdir(path)
 
-def close_window():
-    mymixer.stop()
-    window.quit()
-    window.destroy()
-    # sys.path.remove('./')
 
-#Initialize Window
-window = gui.Tk()
-window.title('Basic Pygame Music Player')
-window.protocol('WM_DELETE_WINDOW', close_window)
-height = 37
-window.geometry('450x%s+300+300' %str(height))
-#Define Sound Files
-set_dir('./sounds')
-#Initialize Mixer
-mymixer = pygame.mixer
-mymixer.init()
-#Create Sound Panels
-track_list = os.listdir(os.getcwd())
-for track_name in track_list:
-    extension = track_name.split('.')[-1]
-    if  extension in VALIDEXTENSIONS:
-        SoundPanel(window, mymixer, track_name).pack()
-        height += 37
-        window.geometry('450x%s+300+300' %str(height))
-    
-window.mainloop()
+def __main__(soundDir):
+    def close_window():
+        mymixer.stop()
+        window.quit()
+        window.destroy()
+        # sys.path.remove('./')
+    #Initialize Window
+    window = gui.Tk()
+    window.title('Basic Pygame Music Player')
+    window.protocol('WM_DELETE_WINDOW', close_window)
+    height = 37
+    window.geometry('450x%s+300+300' %str(height))
+    #Initialize Mixer
+    mymixer = pygame.mixer
+    mymixer.init()
+    #Define Sound Files
+    set_dir(soundDir)
+    #Create Sound Panels
+    track_list = os.listdir(os.getcwd())
+    for track_name in track_list:
+        extension = track_name.split('.')[-1]
+        if  extension in VALIDEXTENSIONS:
+            SoundPanel(window, mymixer, track_name).pack()
+            height += 37
+            window.geometry('450x%s+300+300' %str(height))
+    # Run Window    
+    window.mainloop()
+
+__main__('./sounds')
